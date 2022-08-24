947d4927b86d355340616aeb5007e4079b793898
Joaquin Clausen <jlclausen99@gmail.com>
2022-08-24 11:28:15 -0300

    creadas.js y crear.js finalizados

--- js/creadas.js
+++ js/creadas.js
@@ -11,23 +11,26 @@ if (listaElementosGuardados==""){
     //llevo los elementos guardados a la pagina "creadas.html":
     //consigo el campo:
     let campo = document.getElementById("campo");
-    //creo la fila:
-    let tr = document.createElement("tr");
-    //creo las 4 columnas:
-    let td1 = document.createElement("td");
-    let td2 = document.createElement("td");
-    let td3 = document.createElement("td");
-    let td4 = document.createElement("td");
     //cargo cada columna con su respectivo valor:
     for (const elemento of listaElementos){
+        //creo la fila:
+        let tr = document.createElement("tr");
+        //creo las 4 columnas:
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let td3 = document.createElement("td");
+        let td4 = document.createElement("td");
+        //le doy valores a cada columna:
         td1.innerHTML=`${elemento.nombre}`;
         td2.innerHTML=`${elemento.fechaInicio}`;
         td3.innerHTML=`${elemento.fechaFin}`;
         td4.innerHTML=`${elemento.calificacion}`;
+        //pego cada columna en la fila:
         tr.append(td1);
         tr.append(td2);
         tr.append(td3);
         tr.append(td4);
+        //pego la fila en el campo:
         campo.append(tr);
     }
 }
--- js/crear.js
+++ js/crear.js
@@ -1,14 +1,23 @@
-//TENGO QUE TRAER LA LISTA VIEJA PORQUE NO LA ESTOY USANDO PARA ACTUALIZARLA!!!!
 const listaElementos = [];
-//conseguir el evento formulario:
-let miFormulario = document.getElementById("formulario");
-miFormulario.addEventListener("submit", crearFila);
-function crearFila(e){
-    e.preventDefault();
-    let form = e.target;
-    //creo el objeto y lo meto el objeto a la lista de elementos:
-    listaElementos.push(new Elemento(form.children[1].value, form.children[4].value, form.children[7].value, form.children[10].value, Math.random()));
-    //guardo la listaElementos actual:
-    const enJSON = JSON.stringify(listaElementos);
-    localStorage.setItem("listaElementos", enJSON);
+//Lista de elementos vieja:
+let listaElementosGuardados = localStorage.getItem("listaElementos");
+if (listaElementosGuardados==""){
+    //creo la listaElementos en caso de que no exista:
+    localStorage.setItem("listaElementos", listaElementos);
+}else{
+    //en el caso de que si exista cargo los elementos a mi pagina "creadas.html"
+    listaElementosGuardados = JSON.parse(localStorage.getItem("listaElementos"));
+    for (const elemento of listaElementosGuardados) listaElementos.push(elemento);
+    //conseguir el evento formulario:
+    let miFormulario = document.getElementById("formulario");
+    miFormulario.addEventListener("submit", crearFila);
+    function crearFila(e){
+        e.preventDefault();
+        let form = e.target;
+        //creo el objeto y lo meto el objeto a la lista de elementos:
+        listaElementos.push(new Elemento(form.children[1].value, form.children[4].value, form.children[7].value, form.children[10].value, Math.random()));
+        //guardo la listaElementos actual:
+        const enJSON = JSON.stringify(listaElementos);
+        localStorage.setItem("listaElementos", enJSON);
+    }
 }
