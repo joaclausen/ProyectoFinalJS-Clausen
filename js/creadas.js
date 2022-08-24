7dc39dc0ceaf8e7f3760954cd8a77fc3ea4d85fe
Joaquin Clausen <jlclausen99@gmail.com>
2022-08-24 19:22:22 -0300

    commit final
    mejoras esperables en proxima version:
    *pagina "about" para explicar funcionamiento del sitio
    *que la id aleatoria que no se repita
    *que el usuario pueda cargar una imagen
    *que el usuario pueda guardar su listado como excel
    *apartado para que el usuario escriba una review
    *que el usuario no pueda ingresar por su cuenta un numero menor que 1 o mayor que 10
    *mas funciones para no repetir tanto codigo (soy conciente de que podria ser mucho mas eficiente el codigo)

--- js/creadas.js
+++ js/creadas.js
@@ -1,7 +1,22 @@
 const listaElementos = [];
+//Cantidad de dias anterior:
+let dias = localStorage.getItem("diasTotales");
+//declaro la suma de todas las calificaciones y la variable que contendra el tamaño de la lista (para hacer el promedio):
+let calificaciones = localStorage.getItem("calificaciones");
+let largoLista;
+//si no existe, lo guardo inicializado con 0 en storage:
+if (calificaciones==null || calificaciones==""){
+    calificaciones = 0;
+    localStorage.setItem("calificaciones", calificaciones);
+}
+//si no existe lo inicializo en 0 y lo guardo en storage
+if(dias==null || dias==""){
+    dias=0;
+    localStorage.setItem("diasTotales", dias);
+}
 //cargo listaElementos con posibles valores anteriores:
 let listaElementosGuardados = localStorage.getItem("listaElementos");
-if (listaElementosGuardados==""){
+if (listaElementosGuardados==null || listaElementosGuardados==""){
     //creo la listaElementos en caso de que no exista:
     localStorage.setItem("listaElementos", listaElementos);
 }else{
@@ -33,5 +48,26 @@ if (listaElementosGuardados==""){
         //pego la fila en el campo:
         campo.append(tr);
     }
+    //linea de html con los dias totales:
+    let days = document.getElementById("days");
+    days.innerHTML=`Dias Totales: ${dias}`;
+    //largoLista para la division del promedio:
+    largoLista=listaElementos.length;
+    //linea de html con el promedio (si el largo del array es 0, entonces no se divide):
+    if (largoLista>0){
+        calificaciones = parseInt(calificaciones);
+        calificaciones = (calificaciones / largoLista).toFixed(2);
+        let average = document.getElementById("average");
+        average.innerHTML=`Promedio Calificaciones: ${calificaciones}`;
+    }
+
 }
-//a partir de aca comienza el codigo para calcular promedio de dias y calificaciones:
+//borrado de datos:
+let borrar = document.getElementById("borrar");
+borrar.addEventListener("click", borrarLista);
+function borrarLista(){
+    localStorage.removeItem("listaElementos");
+    localStorage.removeItem('diasTotales');
+    localStorage.removeItem('calificaciones');
+    location.reload()
+}
--- js/crear.js
+++ js/crear.js
@@ -1,13 +1,18 @@
 const listaElementos = [];
 //Lista de elementos vieja:
 let listaElementosGuardados = localStorage.getItem("listaElementos");
-if (listaElementosGuardados==""){
+//Cantidad de dias anterior:
+let dias = parseInt(localStorage.getItem("diasTotales"));
+//Calificaciones anteriores:
+let calificaciones = localStorage.getItem("calificaciones");
+//asigno 0 dias en caso de que diasTotales no exista
+if(dias==null || dias=="" || dias==0) dias=0;
+//lo mismo para las calificaciones:
+if (calificaciones==null || calificaciones=="") calificaciones = 0;
+//Codigo del generamiento de la lista, un caso por si no existe y otro por si ya existe:
+if (listaElementosGuardados==null || listaElementosGuardados==""){
     //creo la listaElementos en caso de que no exista:
     localStorage.setItem("listaElementos", listaElementos);
-}else{
-    //en el caso de que si exista cargo los elementos a mi pagina "creadas.html"
-    listaElementosGuardados = JSON.parse(localStorage.getItem("listaElementos"));
-    for (const elemento of listaElementosGuardados) listaElementos.push(elemento);
     //conseguir el evento formulario:
     let miFormulario = document.getElementById("formulario");
     miFormulario.addEventListener("submit", crearFila);
@@ -16,8 +21,61 @@ if (listaElementosGuardados==""){
         let form = e.target;
         //creo el objeto y lo meto el objeto a la lista de elementos:
         listaElementos.push(new Elemento(form.children[1].value, form.children[4].value, form.children[7].value, form.children[10].value, Math.random()));
+        //actualizo las calificaciones:
+        calificaciones = calificaciones + form.children[10].value;
+        localStorage.setItem("calificaciones", calificaciones);
+        //actualizo los dias:
+        let inicio = new Date (form.children[7].value);
+        let fin = new Date (form.children[4].value);
+        let finInicio = Math.abs((fin - inicio)/86400000);
+        dias = dias + finInicio;
+        //actualizo la cantidad de dias totales:
+        localStorage.setItem("diasTotales", dias);
         //guardo la listaElementos actual:
         const enJSON = JSON.stringify(listaElementos);
         localStorage.setItem("listaElementos", enJSON);
+    }
+    let boton = document.getElementById("botonSubmit");
+    boton.addEventListener("click", exito);
+    function exito(){
+        //recargo pagina y aviso que se cargo correctamente:
+        location.reload();
+        alert("Elemento creado correctamente!");
+    }
+}else{
+    //en el caso de que si exista cargo los elementos a mi pagina "creadas.html"
+    listaElementosGuardados = JSON.parse(localStorage.getItem("listaElementos"));
+    for (const elemento of listaElementosGuardados){
+        listaElementos.push(elemento);
+    }
+    //conseguir el evento formulario:
+    let miFormulario = document.getElementById("formulario");
+    miFormulario.addEventListener("submit", crearFila);
+    function crearFila(e){
+        e.preventDefault();
+        let form = e.target;
+        //creo el objeto y lo meto el objeto a la lista de elementos:
+        listaElementos.push(new Elemento(form.children[1].value, form.children[4].value, form.children[7].value, form.children[10].value, Math.random()));
+        //actualizo las calificaciones:
+        calificaciones = parseInt(calificaciones);
+        calificaciones = calificaciones + parseInt(form.children[10].value);
+        localStorage.setItem("calificaciones", calificaciones);
+        //actualizo los dias:
+        let inicio = new Date (form.children[7].value);
+        let fin = new Date (form.children[4].value);
+        let finInicio = Math.abs((fin - inicio)/86400000);
+        dias = dias + finInicio;
+        //actualizo la cantidad de dias totales:
+        localStorage.setItem("diasTotales", dias);
+        //guardo la listaElementos actual:
+        const enJSON = JSON.stringify(listaElementos);
+        localStorage.setItem("listaElementos", enJSON);
+    }
+    let boton = document.getElementById("botonSubmit");
+    boton.addEventListener("click", exito);
+    function exito(){
+        //recargo pagina y aviso que se cargo correctamente:
+        location.reload();
+        alert("Elemento creado correctamente!");
     }
 }
