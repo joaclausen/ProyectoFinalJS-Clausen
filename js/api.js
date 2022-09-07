fd54a530e44b3bec838e9e1c34e3cacf5c35a659
Joaquin Clausen <jlclausen99@gmail.com>
2022-09-06 21:02:47 -0300

    Actualizacion de api.js
    ahora ya no salta error en una segunda busqueda

--- js/api.js
+++ js/api.js
@@ -8,39 +8,77 @@ const options = {
 };
 
 //obtengo el resultado de la busqueda:
+
 let buscador = document.getElementById("buscador");
+
 buscador.addEventListener("submit", buscarElemento);
+
 function buscarElemento(e){
+
     e.preventDefault();
+
     let texto = e.target;
+
     //obtengo el texto ingresado:
+
     let textoBuscado = texto.children[0].value;
+
+    //dato importante siempre que llames a la funcion buscar a limpiar el dom para agregarle los nuevos elementos
+
+    //de esta forma te evitas codigo de mas incluyendo ese linea aca y listo
+
+    document.querySelector('#pelis').innerHTML = "";
+
     //lo concateno con el url de la api:
+
     fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q='+textoBuscado, options)
- 	.then(response => response.json())
- 	.then(data => {
+
+    .then(response => response.json())
+
+    .then(data => {
+
         //creacion de los elementos que conformaran la lista ()
+
         const list = data.d;
+
         list.map((item)=>{
+
             //l representa el nombre, i la imagen, y el año. Luego agarro y lo meto en codigo de html y lo concateno a mi documento de html:
+
             const nombre = item.l;
-            const imagen = item.i.imageUrl;
+
+            //le puse el operador ? para que no se rompa, ya que note varias busquedas se rompian x que todos tienen imagen
+
+            const imagen = item.i?.imageUrl;
+
             const anio = item.y;
-            const resultado = `<li><img src="${imagen}"> <h2>${nombre}</h2><br><h3>${anio}</h3></li>`
-            document.querySelector('#pelis').innerHTML += resultado;
+
+            //en caso de que si tengan imagen recien ahi lo imprimimos en el dom
+
+            if(imagen != undefined){
+
+                const resultado = `<li><img src="${imagen}"> <h2>${nombre}</h2><br><h3>${anio}</h3></li>`
+
+                document.querySelector('#pelis').innerHTML += resultado;
+
+            }
+
         })
-        //si el usuario hace otra busqueda limpio la pantalla (esto si bien funciona me tira un error del catch por alguna razon):
-        let boton = document.getElementById("boton");
-        boton.addEventListener("click", limpiar);
-        function limpiar(){
-            document.querySelector('#pelis').innerHTML = "";
-        }
+
     })
- 	.catch(err =>{
+
+    .catch(err =>{
+
         Swal.fire({
+
             title: 'No encontrado',
+
             icon: 'error',
+
             text: 'El elemento ingresado no ha sido encontrado',
+
         });
+
     });
+
 }
